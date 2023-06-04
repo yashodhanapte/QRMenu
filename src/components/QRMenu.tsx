8fd8c569dcd301c2fc4a36f53b69accf49f01e89
Yashodhan <yashodhan@Yashodhans-MacBook-Pro.local>
2023-06-04 10:19:19 +0530

    first commit

---
+++ src/components/QRMenu.tsx
@@ -0,0 +1,141 @@
+import React from 'react';
+import './QRMenu.css';
+
+const QRMenu = () => {
+    return (
+        <div className="interior-section-two menu-section">
+
+  <div className="menu-list menu-list__dotted menu-list-1">
+    <h2 className="menu-list__title">Starters</h2>
+    <hr className="separator" />
+    <ul className="menu-list__items">
+            
+        <li className="menu-list__item">
+          <h4 className="menu-list__item-title">
+          <span className="item_title">Soup</span>
+            <span className="dots"></span>
+          </h4>
+          <p className="menu-list__item-desc">
+            <span className="desc__content"></span></p><p>Lorem ipsum dolor sit amet</p>
+
+          <p></p> 
+          <span className="dots"></span>
+          <span className="menu-list__item-price"><p>8</p>
+</span>
+        </li>
+      
+        <li className="menu-list__item">
+          <h4 className="menu-list__item-title">
+          <span className="item_title">Pasta</span>
+            <span className="dots"></span>
+          </h4>
+          <p className="menu-list__item-desc">
+            <span className="desc__content"></span></p><p>Lorem ipsum dolor sit amet</p>
+
+          <p></p> 
+          <span className="dots"></span>
+          <span className="menu-list__item-price"><p>18</p>
+</span>
+        </li>
+      
+        <li className="menu-list__item">
+          <h4 className="menu-list__item-title">
+          <span className="item_title">Salad</span>
+            <span className="dots"></span>
+          </h4>
+          <p className="menu-list__item-desc">
+            <span className="desc__content"></span></p><p>Lorem ipsum dolor sit amet</p>
+
+          <p></p> 
+          <span className="dots"></span>
+          <span className="menu-list__item-price"><p>16</p>
+</span>
+        </li>
+      
+        <li className="menu-list__item">
+          <h4 className="menu-list__item-title">
+          <span className="item_title">Burger</span>
+            <span className="dots"></span>
+          </h4>
+          <p className="menu-list__item-desc">
+            <span className="desc__content"></span></p><p>Grass-fed beef, Vermont cheddar, carmelized onions, brioche bun</p>
+
+          <p></p> 
+          <span className="dots"></span>
+          <span className="menu-list__item-price"><p>12</p>
+</span>
+        </li>
+          </ul>
+  </div>
+
+  <div className="menu-list menu-list__dotted menu-list-2">
+    <h2 className="menu-list__title">Entrees</h2>
+    <hr className="separator" />
+    <ul className="menu-list__items">
+            
+        <li className="menu-list__item">
+          <h4 className="menu-list__item-title">
+          <span className="item_title">Soup</span>
+            <span className="dots"></span>
+          </h4>
+          <p className="menu-list__item-desc">
+            <span className="desc__content"></span></p><p>Lorem ipsum dolor sit amet</p>
+
+          <p></p> 
+          <span className="dots"></span>
+          <span className="menu-list__item-price"><p>8</p>
+</span>
+        </li>
+      
+        <li className="menu-list__item">
+          <h4 className="menu-list__item-title">
+          <span className="item_title">Pasta</span>
+            <span className="dots"></span>
+          </h4>
+          <p className="menu-list__item-desc">
+            <span className="desc__content"></span></p><p>Lorem ipsum dolor sit amet</p>
+
+          <p></p> 
+          <span className="dots"></span>
+          <span className="menu-list__item-price"><p>18</p>
+</span>
+        </li>
+      
+        <li className="menu-list__item">
+          <h4 className="menu-list__item-title">
+          <span className="item_title">Salad</span>
+            <span className="dots"></span>
+          </h4>
+          <p className="menu-list__item-desc">
+            <span className="desc__content"></span></p><p>Lorem ipsum dolor sit amet</p>
+
+          <p></p> 
+          <span className="dots"></span>
+          <span className="menu-list__item-price"><p>16</p>
+</span>
+        </li>
+      
+        <li className="menu-list__item">
+          <h4 className="menu-list__item-title">
+          <span className="item_title">Burger</span>
+            <span className="dots"></span>
+          </h4>
+          <p className="menu-list__item-desc">
+            <span className="desc__content"></span></p><p>Grass-fed beef, Vermont cheddar, carmelized onions, brioche bun</p>
+
+          <p></p> 
+          <span className="dots"></span>
+          <span className="menu-list__item-price"><p>12</p>
+</span>
+        </li>
+          </ul>
+  </div>
+
+
+
+
+</div>
+    )
+}
+
+export default QRMenu;
